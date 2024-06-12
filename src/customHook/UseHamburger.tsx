765d604aa6c837de3dedc0e76a90d9a85a118532
riku <riku62560@gmail.com>
2024-06-12 22:01:56 +0900

    material uiのハンバーガーメニューを導入した

--- src/customHook/UseHamburger.tsx
+++ src/customHook/UseHamburger.tsx
@@ -1,8 +1,27 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
+import HelpIcon from "@mui/icons-material/Help";
+import EmailIcon from "@mui/icons-material/Email";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import EventRepeatIcon from "@mui/icons-material/EventRepeat";
+import FeedIcon from "@mui/icons-material/Feed";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
 const UseHamburger = () => {
-    const [isHamburger , setIsHamburger] = useState(false)
-  return {isHamburger , setIsHamburger}
-}
+  const [isOpenHamburger, setIsOpenHamburger] = useState(false);
+  const toggleHamburger = (newOpen: boolean) => () => {
+    setIsOpenHamburger(newOpen);
+  };
+  const HamburgerList = [
+    { text: "search", icon: <SearchIcon /> },
+    { text: "Home", icon: <HomeIcon /> },
+    { text: "adBee" },
+    { text: "質問", icon: <HelpIcon /> },
+    { text: "解説", icon: <FeedIcon /> },
+    { text: "進捗", icon: <EventRepeatIcon /> },
+    { text: "メッセージ", icon: <EmailIcon /> },
+    { text: "マイページ", icon: <AccountBoxIcon /> },
+  ];
+  return { isOpenHamburger, toggleHamburger, HamburgerList };
+};
 
-export default UseHamburger
+export default UseHamburger;
--- src/pages/messagePage.tsx
+++ src/pages/messagePage.tsx
@@ -1,27 +1,14 @@
-import Hamburger from "@/components/Hamburger";
-import UseHamburger from "@/customHook/UseHamburger";
+
+import HamburgerMenu from "@/components/Hamburger";
 import React, { useState } from "react";
 
 const MessagePage = () => {
-  const { isHamburger, setIsHamburger } = UseHamburger();
-  const displayHamburger = () => {
-    if (isHamburger) {
-      return <Hamburger />;
-    } else {
-      return "rr";
-    }
-  };
+
+
   return (
     <>
-      <button
-        onClick={() => {
-          setIsHamburger((prev) => !prev);
-          console.log("ff");
-        }}
-      >
-        ボタン
-      </button>
-      {displayHamburger()}
+    <HamburgerMenu/>
+
     </>
   );
 };
