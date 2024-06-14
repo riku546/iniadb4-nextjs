c3f8a0f262913ab8cd51fe8e030616c1b1825a98
riku546 <91833521+riku546@users.noreply.github.com>
2024-06-14 13:45:57 +0900

    検索ページにハンバーガーメニューを追加した

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -5,44 +5,50 @@ import Selecter from "@/components/Selecter";
 import NextLink from "next/link";
 import UseSearchPage from "@/customHook/UseSearchPage";
 import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import UnderMenu from "@/components/UnderMenu";
 const SearchPage = () => {
   const { categoryList, searchSuggests, Selectedcategory, setCategoryFn } =
     UseSearchPage();
   return (
-    <div className={styles.container}>
-      <div className={styles.searchArea}>
-        <div className={styles.flexLayout}>
-          <div className={styles.arrowCircleLeftIcon}>
-            <NextLink href="/">
-              <ArrowCircleLeftIcon />
-            </NextLink>
-          </div>
-          <div>
-            <Selecter
-              CategoryList={categoryList}
-              handleChange={setCategoryFn}
-              Selectedcategory={Selectedcategory}
-            />
-          </div>
-          <div className={styles.searchBox}>
-            <Search />
+    <>
+      <div className={styles.container}>
+        <div className={styles.searchArea}>
+          <div className={styles.flexLayout}>
+            <div className={styles.arrowCircleLeftIcon}>
+              <NextLink href="/">
+                <ArrowCircleLeftIcon />
+              </NextLink>
+            </div>
+            <div>
+              <Selecter
+                CategoryList={categoryList}
+                handleChange={setCategoryFn}
+                Selectedcategory={Selectedcategory}
+              />
+            </div>
+            <div className={styles.searchBox}>
+              <Search />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={styles.SearchSuggests}>
-        <b>検索候補タグ</b>
-        <div className={styles.gridLayout}>
-          {searchSuggests[Selectedcategory].map(
-            (category: String, categoryIndex: number) => (
-              <p key={categoryIndex} className={styles.categoryTag}>
-                {category}
-              </p>
-            )
-          )}
+        <div className={styles.SearchSuggests}>
+          <b>検索候補タグ</b>
+          <div className={styles.gridLayout}>
+            {searchSuggests[Selectedcategory].map(
+              (category: String, categoryIndex: number) => (
+                <p key={categoryIndex} className={styles.categoryTag}>
+                  {category}
+                </p>
+              )
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      <div className={styles.underMenu}>
+        <UnderMenu />
+      </div>
+    </>
   );
 };
 
