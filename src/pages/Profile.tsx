0f7c8754ecb615f6309bed05c8d5108bbb7b4b04
riku546 <91833521+riku546@users.noreply.github.com>
2024-06-14 12:56:17 +0900

    プロフィールページのレスポンシブ化をした

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,51 +4,59 @@ import NavList from "@/components/NavList";
 import AndroidIcon from "@mui/icons-material/Android";
 import { Button } from "@mui/material";
 import UseProfile from "@/customHook/UseProfile";
+import UnderMenu from "@/components/UnderMenu";
 
 const profile = () => {
-  const {contentCate , setDisplayMode , displayContent} = UseProfile()
+  const { contentCate, setDisplayMode, displayContent } = UseProfile();
   return (
-    <div className={styles.container}>
-      <NavList />
+    <>
+      <div className={styles.container}>
+        <div className={styles.navbar}>
+          <NavList />
+        </div>
 
-      <div className={styles.UserInfo}>
-        <div className={styles.UserInfoContainer}>
-          <div className={styles.infos}>
-            <AndroidIcon className={styles.icon} />
-            <div>
-              <div className={styles.nameAndLevel}>
-                <b>Enoki</b>
-                <p>Lv 10</p>
-              </div>
-              <div className={styles.follows}>
-                <p>フォロ- 10</p>
-                <p>フォロワー 10</p>
+        <div className={styles.UserInfo}>
+          <div className={styles.UserInfoContainer}>
+            <div className={styles.infos}>
+              <AndroidIcon className={styles.icon} />
+              <div>
+                <div className={styles.nameAndLevel}>
+                  <b>Enoki</b>
+                  <p>Lv 10</p>
+                </div>
+                <div className={styles.follows}>
+                  <p>フォロ- 10</p>
+                  <p>フォロワー 10</p>
+                </div>
               </div>
             </div>
+            <ul className={styles.badges}>
+              <li>8期生</li>
+              <li>IGC2</li>
+              <li>ArtWorks</li>
+            </ul>
           </div>
-          <ul className={styles.badges}>
-            <li>8期生</li>
-            <li>IGC2</li>
-            <li>ArtWorks</li>
-          </ul>
-        </div>
-        <div>
-          <div className={styles.categoryList}>
-            {contentCate.map((category: string, categoryIndex: number) => (
-              <Button
-                variant="outlined"
-                key={categoryIndex}
-                style={{ borderColor: "orange", color: "orange" }}
-                onClick={() => setDisplayMode(contentCate[categoryIndex])}
-              >
-                {category}
-              </Button>
-            ))}
+          <div>
+            <div className={styles.categoryList}>
+              {contentCate.map((category: string, categoryIndex: number) => (
+                <Button
+                  variant="outlined"
+                  key={categoryIndex}
+                  style={{ borderColor: "orange", color: "orange" }}
+                  onClick={() => setDisplayMode(contentCate[categoryIndex])}
+                >
+                  {category}
+                </Button>
+              ))}
+            </div>
+            {displayContent()}
           </div>
-          {displayContent()}
         </div>
       </div>
-    </div>
+      <div className={styles.underMenu}>
+        <UnderMenu />
+      </div>
+    </>
   );
 };
 
